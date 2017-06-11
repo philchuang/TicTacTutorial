a0f767ddcfefc1d346e8e9835b4dac5bd1b7aabf
Phil Chuang <dev@philchuang.com>
2017-06-11 02:04:35 -0400

    created GameManager

---
+++ src/TicTacToe/TicTacToeCore.Tests/GameManagerTests.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Com.PhilChuang.Apps.TicTacToe.Tests
+{
+    [TestClass]
+    public class GameManagerTests
+    {
+        [TestMethod]
+        public void GameManager_CreateNew_should_return_a_new_game()
+        {
+            // Arrange
+            var mgr = new GameManager();
+
+            // Act
+            var game = mgr.CreateNew();
+
+            // Assert
+            Assert.IsNotNull(game);
+            Assert.IsNotNull(game.Player1, "gamme.Player1");
+            Assert.AreEqual('X', game.Player1.Mark, "game.Player1.Mark");
+            Assert.AreEqual("Player 1", game.Player1.Name, "game.Player1.Name");
+            Assert.IsNotNull(game.Player2, "game.Player2");
+            Assert.AreEqual('O', game.Player2.Mark, "game.Player2.Mark");
+            Assert.AreEqual("Player 2", game.Player2.Name, "game.Player2.Name");
+            Assert.AreEqual("Player 1", game.PlayerTurn, "game.PlayerTurn");
+            Assert.IsFalse(game.IsFinished, "game.IsFinished");
+            Assert.IsFalse(game.IsDraw, "game.IsDraw");
+            Assert.IsNull(game.Winner, "game.Winner");
+            Assert.IsNotNull(game.Board, "game.Board");
+            Assert.IsNull(game.Board.Square1, "game.Board.Square1");
+            Assert.IsNull(game.Board.Square2, "game.Board.Square2");
+            Assert.IsNull(game.Board.Square3, "game.Board.Square3");
+            Assert.IsNull(game.Board.Square4, "game.Board.Square4");
+            Assert.IsNull(game.Board.Square5, "game.Board.Square5");
+            Assert.IsNull(game.Board.Square6, "game.Board.Square6");
+            Assert.IsNull(game.Board.Square7, "game.Board.Square7");
+            Assert.IsNull(game.Board.Square8, "game.Board.Square8");
+            Assert.IsNull(game.Board.Square9, "game.Board.Square9");
+        }
+    }
+}
---
+++ src/TicTacToe/TicTacToeCore/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Com.PhilChuang.Apps.TicTacToe
+{
+    public class GameManager
+    {
+        public Game CreateNew()
+        {
+            return new Game
+            {
+                Player1 = new Player
+                {
+                    Mark = 'X',
+                    Name = "Player 1",
+                },
+                Player2 = new Player
+                {
+                    Mark = 'O',
+                    Name = "Player 2",
+                },
+                PlayerTurn = "Player 1",
+                IsFinished = false,
+                IsDraw = false,
+                Winner = null,
+                Board = new Board
+                {
+                    Square1 = null,
+                    Square2 = null,
+                    Square3 = null,
+                    Square4 = null,
+                    Square5 = null,
+                    Square6 = null,
+                    Square7 = null,
+                    Square8 = null,
+                    Square9 = null,
+                }
+            };
+        }
+    }
+}
