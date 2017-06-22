5b643e5bb340528a84034ed6e40b14f01776b405
Phil Chuang <dev@philchuang.com>
2017-06-22 02:45:02 -0400

    refactored GameManager

--- src/TicTacToe/TicTacToeCore/GameManager.cs
+++ src/TicTacToe/TicTacToeCore/GameManager.cs
@@ -19,9 +19,11 @@ namespace Com.PhilChuang.Apps.TicTacToe
 
         public Game CreateNew(string player1Name = "Player 1", string player2Name = "Player 2")
         {
-            var player1 = new Player(player1Name, 'X');
-            var player2 = new Player(player2Name, 'O');
+            return CreateNew(new Player(player1Name, 'X'), new Player(player2Name, 'O'));
+        }
 
+        public Game CreateNew(Player player1, Player player2)
+        {
             return new Game
             {
                 Player1 = player1,
@@ -30,18 +32,13 @@ namespace Com.PhilChuang.Apps.TicTacToe
             };
         }
 
-    public Game CreateNewRandom(string playerName, string otherPlayerName)
-    {
-        var swap = this.RandomProvider.Get(2) == 1;
-        var player1 = new Player(swap ? otherPlayerName : playerName, 'X');
-        var player2 = new Player(swap ? playerName : otherPlayerName, 'O');
-
-        return new Game
+        public Game CreateNewRandom(string playerName, string otherPlayerName)
         {
-            Player1 = player1,
-            Player2 = player2,
-            PlayerTurn = player1,
-        };
-    }
+            var swap = this.RandomProvider.Get(2) == 1;
+            var player1 = new Player(swap ? otherPlayerName : playerName, 'X');
+            var player2 = new Player(swap ? playerName : otherPlayerName, 'O');
+
+            return CreateNew(player1, player2);
+        }
     }
 }
