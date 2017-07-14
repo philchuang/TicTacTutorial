fa303fc44f2a3f081b447e6effd47ab193a0612e
Phil Chuang <dev@philchuang.com>
2017-07-14 01:26:57 -0400

    adding unsaved changes

--- src/TicTacToe/TicTacToeCore.Tests/Testable/GameManagerTestable.cs
+++ src/TicTacToe/TicTacToeCore.Tests/Testable/GameManagerTestable.cs
@@ -9,7 +9,7 @@ namespace Com.PhilChuang.Apps.TicTacToe.Tests.Testable
 {
     public class GameManagerTestable : GameManager
     {
-        // NOTE this could also just be a plain RandomProviderBase property instead of a lambda, but I like lambdas because it has more flexibility
+        // NOTE this could also just be a plain RandomProviderBase property instead of a lambda, but I like lambdas because there's more flexibility
         public Func<RandomProviderBase> MakeRandomProviderOverride { get; set; }
 
         protected override RandomProviderBase MakeRandomProvider()
